efcf7ea593549331f13f3e696f459aa3a86039a9
smsievers <seth_sievers@yahoo.com>
2020-09-14 13:01:27 -0500

    added navbar stuff and basic routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipelistComponent } from './recipelist/recipelist.component';
+import { RecipepageComponent } from './recipepage/recipepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'home', component: HomeComponent},
+{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path: 'recipelist', component: RecipelistComponent},
+{path: 'recipe/:id', component: RecipepageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RecipepageComponent } from './recipepage/recipepage.component';
+import { RecipelistComponent } from './recipelist/recipelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RecipepageComponent,
+    RecipelistComponent
   ],
   imports: [
     BrowserModule,
